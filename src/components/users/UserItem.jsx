ebb63a3efae0fc4352a95c9ec6cd350cf2952d8c
Miquel Espinosa <miquelespinosa2@gmail.com>
2022-04-12 17:14:55 +0200

    setting up the context

--- src/components/users/UserItem.jsx
+++ src/components/users/UserItem.jsx
@@ -8,8 +8,8 @@ function UserItem({user: { login, avatar_url }}) {
       <div className="flex-row items-center space-x-4 card-body">
         <div>
           <div className="avatar">
-            <div className="rounded-ful shadow w-14 h-14">
-              <img src={avatar_url} alt="profile pic" />
+            <div className="rounded-full shadow w-14 h-14">
+              <img src={avatar_url} alt="profile pic"/>
             </div>
           </div>
         </div>
