0b54a1bffe63c4510ac8abab251ffeae2b1e4b61
Miquel Espinosa <miquelespinosa2@gmail.com>
2022-04-12 17:29:42 +0200

    cleaning up the code

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -8,13 +8,15 @@ const GITHUB_URL = process.env.REACT_APP_GITHUB_URL
 export const GithubProvider = ({children}) => {
  const initialState = {
    users: [],
-   loading: true
+   loading: false
  }
 
 
  const [state, dispatch] = useReducer(githubReducer, initialState)
 
   const fetchUsers = async () => {
+    setLoading()
+    
     const response = await fetch(`${GITHUB_URL}/users`)
 
     const data = await response.json()
@@ -25,6 +27,9 @@ export const GithubProvider = ({children}) => {
     })
 
   }
+
+  // set loading
+  const setLoading = () => dispatch({type: 'SET_LOADING'})
 
   return <GithubContext.Provider value={{
     users: state.users,
