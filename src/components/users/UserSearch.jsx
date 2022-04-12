91d1c76934110ba6a43b56947578c15bbf59d9c1
Miquel Espinosa <miquelespinosa2@gmail.com>
2022-04-12 17:49:03 +0200

    handled the search user events

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -5,7 +5,7 @@ import GithubContext from '../../context/github/GithubContext'
 function UserSearch() {
   const [text, setText] = useState('')
 
-  const {users} = useContext(GithubContext)
+  const {users, searchUsers} = useContext(GithubContext)
 
   const handleChange = (e) => setText(e.target.value)
 
@@ -15,7 +15,7 @@ function UserSearch() {
     if(text === '') {
       alert('Please enter something')
     } else {
-      // @todo - search users
+      searchUsers(text)
 
       setText('')
     }
@@ -40,11 +40,11 @@ function UserSearch() {
       </form>
       </div>
       {users.length > 0 && (
-              <div>
-              <button className="btn btn-ghost btn-lg">
-                Clear
-              </button>
-            </div>
+        <div>
+        <button className="btn btn-ghost btn-lg">
+          Clear
+        </button>
+      </div>
       )}
 
     </div>
