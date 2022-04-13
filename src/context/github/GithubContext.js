5ad188735ed208a33c62c41f2b575d7e2c2b32a8
Miquel Espinosa <miquelespinosa2@gmail.com>
2022-04-13 11:14:41 +0200

    added the user profile show page

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -8,6 +8,7 @@ const GITHUB_URL = process.env.REACT_APP_GITHUB_URL
 export const GithubProvider = ({children}) => {
  const initialState = {
    users: [],
+   user: {},
    loading: false
  }
 
@@ -31,6 +32,29 @@ export const GithubProvider = ({children}) => {
     })
 
   }
+  // Get single User
+  const getUser = async (login) => {
+    setLoading()
+
+    const response = await fetch(`${GITHUB_URL}/users/${login}`)
+
+
+    if(response.status === 404) {
+      window.location = '/notfound'
+    } else {
+      const data = await response.json()
+
+      dispatch({
+        type: 'GET_USER',
+        payload: data,
+      })
+
+    }
+
+
+  }
+
+
 
   //clearing users from state
   const clearUsers = () => dispatch({type: 'CLEAR_USERS'})
@@ -41,6 +65,7 @@ export const GithubProvider = ({children}) => {
   return <GithubContext.Provider value={{
     users: state.users,
     loading: state.loading,
+    user: state.user,
     searchUsers,
     clearUsers,
   }}>
