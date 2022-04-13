045b48fb43c70368ac280d66e15f99a7c6cf0f97
Miquel Espinosa <miquelespinosa2@gmail.com>
2022-04-13 11:48:01 +0200

    added public repos and gists

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -142,6 +142,28 @@ function User() {
               {following}
             </div>
           </div>
+          <div className="stat">
+            <div className="stat-figure text-secondary">
+              <FaCodepen className='text-3xl md:text-5xl'/>
+            </div>
+            <div className="stat-title pr-5">
+              Public repos
+            </div>
+            <div className="stat-value pr-5 text-3xl md:text 4xl">
+              {public_repos}
+            </div>
+          </div>
+          <div className="stat">
+            <div className="stat-figure text-secondary">
+              <FaStore className='text-3xl md:text-5xl'/>
+            </div>
+            <div className="stat-title pr-5">
+              Public Gists
+            </div>
+            <div className="stat-value pr-5 text-3xl md:text 4xl">
+              {public_gists}
+            </div>
+          </div>
         </div>
       </div>
     </>
